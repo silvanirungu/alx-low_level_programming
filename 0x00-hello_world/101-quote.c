b4e9e5a97b288f7891fb824cd6d60fd79e4bfadb
Silsa <silsa.russo1@gmail.com>
2023-06-15 09:20:25 -0700

    try

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,9 @@
-#!/bin/bash
-write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n",58);
+unstd.h
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+    return (1);
+}
