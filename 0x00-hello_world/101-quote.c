8529b36c99bf304f801463214e9c2d5566770186
Silsa <silsa.russo1@gmail.com>
2023-06-15 12:26:23 -0700

    try 4

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,7 @@
  */
 int main(void)
 {
-write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+char strl[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, strl,59);
 return (1);
 }
