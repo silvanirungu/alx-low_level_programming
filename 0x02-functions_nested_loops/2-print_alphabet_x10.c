e1ead2a9f45fba7a9c7803e6c001ce24cd414101
Silsa <silsa.russo1@gmail.com>
2023-06-20 01:56:51 -0700

    leave

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+int i;
+char c;
+for (i = 0; i < 10; i++)
+{
+for (c = 'a'; c <= 'z'; c++)
+_putchar(c);
+_putchar('\n');
+print_alphabet_x10();
+return (0);
+}
+}
