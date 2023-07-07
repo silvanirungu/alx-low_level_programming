dcf8e6cf94fa1b03eaead9874d0feffad951327a
Silsa <silsa.russo1@gmail.com>
2023-07-07 05:41:39 -0700

    new try

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main - check the code
+ *@argc:value to be ecaluated.
+ *@argv: value to be evaluated
+ * Return: Always 0.
+ */
 int main(int argc, char *argv[])
 {
 printf("%s\n", argv[0]);
-return 0;
+return (0);
 }
