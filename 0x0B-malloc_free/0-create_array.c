c79ccf7806a774712dd984aeb46ff95afcd8167b
Silsa <silsa.russo1@gmail.com>
2023-07-11 02:37:43 -0700

    idk

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,25 +1,29 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * *create_array - prints a string in reverse.
- * @size: string
- * @c: value to be evaluated
- * Return: no return.
+ * create_array - Main Entry
+ * @size: input
+ * @c: input
+ * Return: 0
  */
 char *create_array(unsigned int size, char c)
 {
+char *arr;
+unsigned int i;
+
 if (size == 0)
 {
 return (NULL);
 }
-char *array = malloc(size * sizeof(char));
-if (array == NULL)
+arr = malloc(sizeof(char) * size);
+
+if (arr == NULL)
 {
 return (NULL);
 }
-for (unsigned int i = 0; i < size; i++)
+for (i = 0; i < size; i++)
 {
-array[i] = c;
+arr[i] = c;
 }
-return (array);
+return (arr);
 }
